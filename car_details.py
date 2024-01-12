4ae579ed2ec40af0fc2c410781a56f02de5436c4
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-12 11:13:00 +0530

    Update and rename 8.py to car_details.py

---
+++ car_details.py
@@ -0,0 +1,15 @@
+# Create a Python class called “Car” with attributes like make, model, and year. Then, create an object of the “Car” class and print its details.
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_details(self):
+        return f"{self.year} {self.make} {self.model}"
+
+carObj = Car(make="Toyota", model="Supra", year=2023)
+
+print("Car Details:")
+print(carObj.get_details())
