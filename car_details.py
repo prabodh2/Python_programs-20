f602d1d2474a9faa83f167809c06056d846e0277
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-18 23:42:47 +0530

    Update car_details.py

--- car_details.py
+++ car_details.py
@@ -5,11 +5,8 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
-
     def get_details(self):
         return f"{self.year} {self.make} {self.model}"
-
 carObj = Car(make="Toyota", model="Supra", year=2023)
-
 print("Car Details:")
 print(carObj.get_details())
