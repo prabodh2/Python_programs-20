bff15a319c5247f37e62724ed0e0cf7ca107d77f
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-18 23:56:17 +0530

    Update Implement_two_subclass.py

--- Implement_two_subclass.py
+++ Implement_two_subclass.py
@@ -3,11 +3,9 @@
 class Vehicle:
     def drive(self):
         return "Generic vehicle driving method"
-
 class Car(Vehicle):
     def drive(self):
         return "Car is driving on the road."
-
 class Bicycle(Vehicle):
     def drive(self):
         return "Bicycle is pedaling on the street."
@@ -15,12 +13,9 @@ class Bicycle(Vehicle):
 vehicleObj = Vehicle()
 carObj = Car()
 bicycleObj = Bicycle()
-
 print("Vehicle:")
 print(vehicleObj.drive())
-
 print("\nCar:")
 print(carObj.drive())
-
 print("\nBicycle:")
 print(bicycleObj.drive())
