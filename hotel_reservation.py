b307d589c80980b164ce19c6db04f794411e488d
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-18 23:47:54 +0530

    Update hotel_reservation.py

--- hotel_reservation.py
+++ hotel_reservation.py
@@ -8,13 +8,11 @@ class Room:
         self.capacity = capacity
         self.rate_per_night = rate_per_night
         self.reservations = []
-
     def check_availability(self, start_date, end_date):
         for reservation in self.reservations:
             if start_date < reservation.check_out_date and end_date > reservation.check_in_date:
                 return False
         return True
-
     def book_room(self, guest, check_in_date, check_out_date):
         if self.check_availability(check_in_date, check_out_date):
             reservation = Reservation(guest, self, check_in_date, check_out_date)
@@ -25,8 +23,6 @@ class Room:
         else:
             print(f"Room {self.room_number} is not available during the specified time period.")
             return False
-
-
 class Guest:
     def __init__(self, guest_id, name, email):
         self.guest_id = guest_id
@@ -36,29 +32,21 @@ class Guest:
 
     def add_reservation(self, reservation):
         self.reservations.append(reservation)
-
-
 class Reservation:
     def __init__(self, guest, room, check_in_date, check_out_date):
         self.guest = guest
         self.room = room
         self.check_in_date = check_in_date
         self.check_out_date = check_out_date
-
     def calculate_total_cost(self):
         nights = (self.check_out_date - self.check_in_date).days
         return nights * self.room.rate_per_night
-
-
 room1 = Room(room_number="101", capacity=2, rate_per_night=100)
 room2 = Room(room_number="102", capacity=4, rate_per_night=150)
-
 guest1 = Guest(guest_id=1, name="u1", email="u1@gmail.com")
 guest2 = Guest(guest_id=2, name="u2", email="u2@gmail.com")
-
 start_date = datetime(2023, 1, 1)
 end_date = datetime(2023, 1, 5)
-
 available = room1.check_availability(start_date, end_date)
 if available:
     print(f"Room {room1.room_number} is available during the specified time period.")
