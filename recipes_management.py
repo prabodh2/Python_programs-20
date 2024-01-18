40bcd6e00875a9bc8603f1fcdb795a73d078c7d1
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2024-01-18 23:49:01 +0530

    Update recipes_management.py

--- recipes_management.py
+++ recipes_management.py
@@ -9,32 +9,24 @@ class Recipe:
         self.title = title
         self.ingredients = ingredients
         self.instructions = instructions
-
     def __str__(self):
         return f"{self.title}\nIngredients: {', '.join(ingredient.name for ingredient in self.ingredients)}\nInstructions: {self.instructions}"
-
-
 class User:
     def __init__(self, username):
         self.username = username
         self.recipes = []
-
     def add_recipe(self, recipe):
         self.recipes.append(recipe)
-
     def search_by_ingredient(self, ingredient_name):
         matching_recipes = []
         for recipe in self.recipes:
             ingredients = [ingredient.name.lower() for ingredient in recipe.ingredients]
             if ingredient_name.lower() in ingredients:
                 matching_recipes.append(recipe)
-
         return matching_recipes
-
 ingredient1 = Ingredient(name="Chicken")
 ingredient2 = Ingredient(name="Rice")
 ingredient3 = Ingredient(name="Tomatoes")
-
 recipe1 = Recipe(title="Chicken Curry", ingredients=[ingredient1, ingredient2, ingredient3], instructions="Cook and enjoy!")
 recipe2 = Recipe(title="Vegetable Stir Fry", ingredients=[ingredient2, ingredient3], instructions="Saute and serve.")
 
